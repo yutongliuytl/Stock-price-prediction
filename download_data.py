1c526beb7a2fb1773a12dcc20a18d00ae6659aa0
William Zhang <w.ykzhang@hotmail.com>
2019-08-29 22:47:42 -0400

    added stock list and initial function to download every stock in stock_list to stocks_data folder

---
+++ download_data.py
@@ -0,0 +1,20 @@
+import datetime as dt
+import pandas_datareader.data as web
+import os
+import csv
+
+file = open('stock_list.txt', 'r', encoding='utf-8-sig')
+for line in file:
+    data = [n for n in line[:-1].split(',')]
+
+folder_name = 'stocks_data'
+for ticker in data:
+    # just in case your connection breaks, we'd like to save our progress!
+    if not os.path.exists('{}/{}.csv'.format(folder_name, ticker)):
+        start = dt.datetime(2000, 1, 1)
+        end = dt.datetime.now()
+        df = web.DataReader(ticker, 'yahoo', start, end)
+        df.to_csv('{}/{}.csv'.format(folder_name, ticker))
+    else:
+        # update the ticker here
+        print('Already have {}'.format(ticker))
